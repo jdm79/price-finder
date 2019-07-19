3e22c67fa4cf9d44f4db9b82fad53842f7d1e764
jdm79 <james.malvern@gmail.com>
2019-07-19 23:32:10 +0100

    refactor: shoves the html tag info into a snippet variable, makes everything a bit cleaner

--- src/app.py
+++ src/app.py
@@ -3,15 +3,16 @@ from bs4 import BeautifulSoup
 
 
 url = "https://www.argos.co.uk/product/8838047"
+html_snippet = {
+    "class": "price product-price-primary",
+    "itemprop": "price",
+    "content": "34.99"
+}
 
 request = requests.get(url)
 content = request.content
 soup = BeautifulSoup(content, "html.parser")
-element = soup.find("li", {
-    "class": "price product-price-primary",
-    "itemprop": "price",
-    "content": "34.99"
-})
+element = soup.find("li", html_snippet)
 string_price = element.text
 
 budget = 30
