7d6708665a6a2174e3e304f784bc34c2a428f3fb
jdm79 <james.malvern@gmail.com>
2019-07-19 23:36:43 +0100

    refactor: cut some of the snippet, put code into function

--- src/app.py
+++ src/app.py
@@ -1,34 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
+def webscraper():
+    url = "https://www.argos.co.uk/product/8838047"
+    html_snippet = {
+        "class": "price product-price-primary",
+        "itemprop": "price"
+    }
 
-url = "https://www.argos.co.uk/product/8838047"
-html_snippet = {
-    "class": "price product-price-primary",
-    "itemprop": "price",
-    "content": "34.99"
-}
+    request = requests.get(url)
+    content = request.content
+    soup = BeautifulSoup(content, "html.parser")
+    element = soup.find("li", html_snippet)
+    string_price = element.text
 
-request = requests.get(url)
-content = request.content
-soup = BeautifulSoup(content, "html.parser")
-element = soup.find("li", html_snippet)
-string_price = element.text
+    budget = 30
+    price = float(string_price[1:])
 
-budget = 30
-price = float(string_price[1:])
+    if price < budget:
+        print("This tent is less than £30 - buy it!")
+    else:
+        print("This tent is too damn expensive!")
+        # else
+        # print("This tent is too expensive. stand down, soldier.")
 
-if price < budget:
-    print("This tent is less than £30 - buy it!")
-else:
-    print("This tent is too damn expensive!")
-    # else
-    # print("This tent is too expensive. stand down, soldier.")
-
-
-
+webscraper()
 
 
+
+# Below is the original HTML snippet from the Argos website
 # <li class="price product-price-primary"
 # itemprop="price" content="34.99">
 # <span itemprop="priceCurrency" content="GBP"></span>£34.99</li>
